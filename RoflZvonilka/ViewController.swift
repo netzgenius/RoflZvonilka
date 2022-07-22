709cea724262ffb32c8eea06a4ce99d510bc503c
Kirilo <fffyffff@gmail.com>
2022-07-22 17:22:00 +0300

    Small fix

--- RoflZvonilka/ViewController.swift
+++ RoflZvonilka/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import Foundation
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //SLIDER
+        slider.minimumValue = 3800000000
+        slider.maximumValue = 3809999999
+        slider.minimumTrackTintColor = .yellow
+        slider.minimumTrackTintColor = .blue
+        slider.thumbTintColor = .gray
+        //LABEL
+        label.text = String(slider.value)
+        
     }
-
-
+//Label
+    @IBAction func sliderAction(_ sender: UISlider) {
+        label.text = String (Int((sender.value))) }
+    //Button
+    @IBAction func callButtonClicked(_ sender: UIButton) {
+        if let url = URL(string: "tel://\(label.text!)"),
+        UIApplication.shared.canOpenURL(url) {
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
 }
 
